ffd4f40aa5403688fb89fce7cf510fbb4313056a
jose-PC\jose <jose.vicente.martinez.diaz@gmail.com>
2017-09-24 23:37:19 +0200

    EventListener

--- BotIsAlive/src/es/technobroken/botisalive/BotIsAlive.java
+++ BotIsAlive/src/es/technobroken/botisalive/BotIsAlive.java
@@ -1,10 +1,37 @@
 package es.technobroken.botisalive;
 
-public class BotIsAlive {
+import com.jogamp.opengl.GLAutoDrawable;
+import com.jogamp.opengl.GLEventListener;
+
+public class BotIsAlive implements GLEventListener {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void init(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void display(GLAutoDrawable drawable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
